b479b40a77afca995ac6dab67ee8100a3b6777ac
Acer <5457892350@my.lerna.kz>
2025-08-12 19:45:24 +0300

    feat: setup security with custom UserDetailsService and AppUserPrincipal

---
+++ src/main/java/com/hotel/security/AppUserPrincipal.java
@@ -0,0 +1,54 @@
+package com.hotel.security;
+
+import com.hotel.entity.Role;
+import com.hotel.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class AppUserPrincipal implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream().map(Role::toAuthority).toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    public Long getId() {
+        return user.getId();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/hotel/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.hotel.security;
+
+import com.hotel.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
+        return new AppUserPrincipal(userService.findByName(name));
+    }
+}
